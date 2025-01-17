aaf33ca58234920c980631a1edc47ed0137b00dc
Dawid Kapcia <dawidkapcia65@gmail.com>
2025-01-17 12:01:59 +0100

    Adding swagger documentation for API

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -2,11 +2,26 @@ from django.contrib import admin
 from django.urls import path, include
 from api.views import CreateUserView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+description = "Welcome to the NeuroScan API\n\nThis API provides essential functionality for user authentication and data management. Designed with REST principles, it enables seamless interaction with the system through standard HTTP methods such as GET, POST, PUT, and DELETE."
+
+schema_view = get_schema_view(openapi.Info(title="NeuroScan API",
+                                           default_version='v1',
+                                           description=description,
+                                           terms_of_service="https://www.google.com/policies/terms/",
+                                           license=openapi.License(name="BSD License")), public=True, permission_classes=[permissions.AllowAny,])
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/user/register/', CreateUserView.as_view(), name='register'),
     path('api/token/', TokenObtainPairView.as_view(), name='get_token'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='refresh_token'),
-    path('api-auth/', include('rest_framework.urls'))
+    path('api-auth/', include('rest_framework.urls')),
+
+    path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
