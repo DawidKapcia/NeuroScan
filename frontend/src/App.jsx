4f6e717b7ef33533cd534facf127442a38db890f
Dawid Kapcia <dawidkapcia65@gmail.com>
2025-01-17 13:12:00 +0100

    Frontend initialization

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+function App() {
+  return (
+    <>
+      <h1>Hello, world!</h1>
+      <p>Welcome to the frontend!</p>
+    </>
+  )
+}
+
+export default App
