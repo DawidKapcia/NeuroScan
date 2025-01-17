2102090333880d7364d6e5868e9f68215a09f012
Dawid Kapcia <dawidkapcia65@gmail.com>
2025-01-17 14:15:17 +0100

    Routing initialization

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,59 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import Login from "./pages/Login"
+import Register from "./pages/Register"
+import Home from "./pages/Home"
+import Start from "./pages/Start"
+import NotFound from "./pages/NotFound"
+import ProtectedRoute from './components/ProtectedRoute'
+
+function Logout() {
+  localStorage.clear()
+  return <Navigate to="/start" />
+}
+
+function RegisterAndLogout() {
+  localStorage.clear()
+  return <Register />
+}
+
+function LoginAndLogout() {
+  localStorage.clear()
+  return <Login />
+}
 
 function App() {
   return (
-    <>
-      <h1>Hello, world!</h1>
-      <p>Welcome to the frontend!</p>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={<ProtectedRoute><Home /></ProtectedRoute>}
+        ></Route>
+
+        <Route
+          path="/login"
+          element={<LoginAndLogout />}
+        ></Route>
+
+        <Route
+          path="/register"
+          element={<RegisterAndLogout />}
+        ></Route>
+
+        <Route
+          path="/start"
+          element={<Start />}
+        ></Route>  
+
+        <Route
+          path="*"
+          element={<NotFound />}
+        ></Route>       
+
+      </Routes>
+    </BrowserRouter>
   )
 }
 
-export default App
+export default App;
---
+++ frontend/src/pages/Start.jsx
@@ -0,0 +1,9 @@
+function Start() {
+    return (
+      <div>
+        <h1>Start</h1>
+      </div>
+    );
+  }
+
+export default Start;
